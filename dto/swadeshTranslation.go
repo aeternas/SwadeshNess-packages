728325e41643c26abd5cf6f1f0f15688c955e8ff
Ivan Golikov <igolikov@octoberry.ru>
2019-03-09 18:40:34 +0300

    Removed unnecessary value mappings

--- dto/swadeshTranslation.go
+++ dto/swadeshTranslation.go
@@ -1,17 +1,17 @@
 package translation
 
 type SwadeshTranslation struct {
-	Results []GroupTranslation `json:"results"`
-	Credits string             `json:"credits"`
+	Results []GroupTranslation
+	Credits string
 }
 
 type LanguageTranslation struct {
-	Name        string `json:"name"`
-	Translation string `json:"translation"`
-	Cached      bool   `json:"isCached"`
+	Name        string
+	Translation string
+	Cached      bool `json:"isCached"`
 }
 
 type GroupTranslation struct {
-	Name    string                `json:"name"`
-	Results []LanguageTranslation `json:"results"`
+	Name    string
+	Results []LanguageTranslation
 }
