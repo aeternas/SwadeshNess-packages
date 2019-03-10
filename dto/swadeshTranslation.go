64d91a81bf2d0f86940f04693b29e2965e85d09a
Ivan Golikov <igolikov@octoberry.ru>
2019-03-10 10:47:30 +0300

    Revert "Removed unnecessary value mappings"
    This reverts commit 728325e41643c26abd5cf6f1f0f15688c955e8ff.

--- dto/swadeshTranslation.go
+++ dto/swadeshTranslation.go
@@ -1,17 +1,17 @@
 package translation
 
 type SwadeshTranslation struct {
-	Results []GroupTranslation
-	Credits string
+	Results []GroupTranslation `json:"results"`
+	Credits string             `json:"credits"`
 }
 
 type LanguageTranslation struct {
-	Name        string
-	Translation string
-	Cached      bool `json:"isCached"`
+	Name        string `json:"name"`
+	Translation string `json:"translation"`
+	Cached      bool   `json:"isCached"`
 }
 
 type GroupTranslation struct {
-	Name    string
-	Results []LanguageTranslation
+	Name    string                `json:"name"`
+	Results []LanguageTranslation `json:"results"`
 }
